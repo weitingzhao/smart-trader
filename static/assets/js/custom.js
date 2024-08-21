71a1c293ed0d870fff54a3ce34aca6b2b734aa53
vision <vsion.zhao@gmail.com>
2024-08-20 21:55:16 -0500

    notification feature read / read all

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,3 +1,19 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 let currentIndex = -1;
 
 function fetchAutoReminder(event) {
@@ -17,26 +33,29 @@ function fetchAutoReminder(event) {
             resultsContainer.innerHTML = '';
             const regex = new RegExp(`(${query})`, 'gi');
             data.forEach((item,index) => {
-                const resultItem = document.createElement('a');
-                resultItem.className = 'list-group-item list-group-item-action';
-                resultItem.setAttribute('data-index', index);
-                resultItem.setAttribute('data-symbol', item.symbol);
+                const listItem = document.createElement('li');
+                listItem.className = 'mb-2';
+                const linkitem = document.createElement('a');
+                linkitem.className = 'dropdown-item border-radius-md';
+                linkitem.setAttribute('data-index', index);
+                linkitem.setAttribute('data-symbol', item.symbol);
                 const highlightedSymbol = item.symbol.replace(regex, '<span class="highlight">$1</span>');
                 const highlightedName = item.name.replace(regex, '<span class="highlight">$1</span>');
-                resultItem.innerHTML = `
+                linkitem.innerHTML = `
                 <div class="result-line1">${highlightedSymbol} - ${highlightedName}</div>
                 <div class="result-line2">${item.market}</div>
               `;
-                resultItem.addEventListener('click', () => {
+                linkitem.addEventListener('click', () => {
                     currentIndex = index;
                     selectResult();
                 });
-                resultItem.addEventListener('mouseover', () => {
+                linkitem.addEventListener('mouseover', () => {
                     // console.log('mouseover:', currentIndex);
                     currentIndex = index;
                     highlightResult();
                 });
-                resultsContainer.appendChild(resultItem);
+                listItem.appendChild(linkitem);
+                resultsContainer.appendChild(listItem);
             });
             resultsContainer.style.display = 'block';
         });
@@ -55,7 +74,8 @@ function handleKeyEvents(event) {
 }
 
 function navigateResults(direction) {
-    const results = document.querySelectorAll('#auto_reminder_results .list-group-item');
+    const results = document.querySelectorAll('#auto_reminder_results .mb-2 .dropdown-item');
+    console.log('Results:', results.length);
     if (results.length === 0) return;
 
     currentIndex = (currentIndex + direction + results.length) % results.length;
@@ -76,14 +96,20 @@ function navigateResults(direction) {
 }
 
 function highlightResult() {
-    const results = document.querySelectorAll('#auto_reminder_results .list-group-item');
+    const results = document.querySelectorAll('#auto_reminder_results .mb-2 .dropdown-item');
     results.forEach((result, index) => {
-        result.classList.toggle('active', index === currentIndex);
+        if (index === currentIndex) {
+            result.classList.add('bg-dark');
+            result.classList.add('text-white');
+        } else {
+            result.classList.remove('bg-dark');
+            result.classList.remove('text-white');
+        }
     });
 }
 
 function selectResult() {
-    const results = document.querySelectorAll('#auto_reminder_results .list-group-item');
+    const results = document.querySelectorAll('#auto_reminder_results .mb-2 .dropdown-item');
     if (currentIndex >= 0 && currentIndex < results.length) {
         console.log('Open Index:', currentIndex);
         const selectedResult = results[currentIndex];
@@ -206,73 +232,205 @@ function showNotification(level, title, content, timeAgo = 'just now') {
 
 document.addEventListener('DOMContentLoaded', function() {
     const notificationButton = document.getElementById('notificationButton');
-    const notificationPanel = document.getElementById('notificationPanel');
-    //
-    // notificationButton.addEventListener('click', function () {
-    //     if (notificationPanel.style.display === 'none' || notificationPanel.style.display === '') {
-    //         notificationPanel.style.display = 'block';
-    //     } else {
-    //         notificationPanel.style.display = 'none';
-    //     }
-    // });
+    const notificationMenu = document.getElementById('notificationMenu');
+    const notificationCount = document.getElementById('notificationCount');
+    const max_notifications = 10;
+    let notificationQueue={};
+    let unread_count = 0;
+    let read_all_link = null;
+    notificationMenu.innerHTML = ''; // Clear existing notifications
 
-    function generateNotificationPanel(notifications) {
-        notifications.forEach(notification => {
-            const listItem = document.createElement('li');
-            listItem.className = 'mb-2';
-            const link = document.createElement('a');
-            link.className = 'dropdown-item border-radius-md';
-            link.href = 'javascript:;';
-            const div1 = document.createElement('div');
-            div1.className = 'd-flex py-1';
-            const div2 = document.createElement('div');
-            div2.className = 'my-auto';
-            // const img = document.createElement('img');
-            // img.src = notification.image;
-            // img.className = 'avatar avatar-sm me-3';
-            // img.alt = 'user image';
+    // Toggle dropdown visibility
+    notificationButton.addEventListener('click', function(event) {
+        notificationMenu.classList.toggle('show');
+        // Toggle aria-expanded attribute
+        let expanded = notificationMenu.classList.contains('show');
+        this.setAttribute('aria-expanded', expanded);
+    });
 
-            const div3 = document.createElement('div');
-            div3.className = 'd-flex flex-column justify-content-center';
-            const h6 = document.createElement('h6');
-            h6.className = 'text-sm font-weight-normal mb-1';
-            h6.innerHTML = `<span class="font-weight-bold">${notification.verb}</span> by ${notification.actor}`;
-            const p = document.createElement('p');
-            p.className = 'text-xs text-secondary mb-0';
-            p.innerHTML = `<i class="fa fa-clock me-1"></i>${notification.timestamp}`;
+    // Close dropdown if clicking outside
+    document.addEventListener('click', function(event) {
+        if (!notificationMenu.contains(event.target) && !notificationButton.contains(event.target)) {
+            notificationMenu.classList.remove('show');
+            notificationButton.setAttribute('aria-expanded', 'false');
+        }
+    });
 
-            div3.appendChild(h6);
-            div3.appendChild(p);
+    // Prevent closing dropdown when clicking inside the menu
+    notificationMenu.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevents the click from bubbling up to the document
+    });
 
-            // div2.appendChild(img);
 
-            div1.appendChild(div2);
-            div1.appendChild(div3);
+    function generateNotificationPanel(notification) {
+        const listItem = document.createElement('li');
+        listItem.className = 'mb-2';
+        const link = document.createElement('a');
+        link.className = 'dropdown-item border-radius-md';
+        link.href = 'javascript:;';
+        const div1 = document.createElement('div');
+        div1.className = 'd-flex py-1';
+        const div2 = document.createElement('div');
+        div2.className = 'my-auto';
+        const img = document.createElement('img');
+        img.src = "/static/assets/img/illustrations/chat.png" //notification.image;
+        img.className = 'avatar avatar-sm me-3';
+        img.alt = 'user image';
+        // Set background color based on notification level
+        switch (notification.level) {
+            case 'success':
+                img.classList.add('bg-gradient-success');
+                break;
+            case 'info':
+                img.classList.add('bg-gradient-info');
+                break;
+            case 'warning':
+                img.classList.add('bg-gradient-warning');
+                break;
+            case 'error':
+                img.classList.add('bg-gradient-danger');
+                break;
+            default:
+                img.classList.add('bg-gradient-info'); // Default to info if level is not recognized
+                break;
+        }
 
-            link.appendChild(div1);
+        const div3 = document.createElement('div');
+        div3.className = 'd-flex flex-column justify-content-center';
+        const h6 = document.createElement('h6');
+        h6.className = 'text-sm font-weight-normal mb-1';
+        h6.innerHTML = `<span class="font-weight-bold">${notification.verb}</span> by ${notification.actor}`;
+        const p = document.createElement('p');
+        p.className = 'text-xs text-secondary mb-0';
+        p.innerHTML = `<i class="fa fa-clock me-1"></i>${notification.timestamp}`;
 
-            listItem.appendChild(link);
-            notificationPanel.appendChild(listItem);
+        const closeButtonWrapper = document.createElement('div');
+        closeButtonWrapper.className = 'close-button-wrapper d-flex justify-content-center align-items-center';
+        closeButtonWrapper.style.width = '24px';
+        closeButtonWrapper.style.height = '24px';
+        closeButtonWrapper.style.cursor = 'pointer';
+
+        // Add close button
+        const closeButton = document.createElement('i');
+        closeButton.className = 'fas fa-times text-md ms-3 cursor-pointer';
+        closeButton.style.margin = 'auto 0'; // Center the button vertically
+        closeButton.addEventListener('click', function() {
+            markAsRead(notification.slug, listItem);
         });
+
+        closeButtonWrapper.appendChild(closeButton);
+
+        div3.appendChild(h6);
+        div3.appendChild(p);
+
+        div2.appendChild(img);
+
+        div1.appendChild(div2);
+        div1.appendChild(div3);
+        div1.appendChild(closeButtonWrapper);
+
+        link.appendChild(div1);
+
+        listItem.appendChild(link);
+        notificationMenu.appendChild(listItem);
+
+        // Add to queue
+        notificationQueue[notification.slug] = notification;
     }
+
+    function markAsRead(slug, listItem) {
+        fetch(`/inbox/notifications/mark-as-read/${slug}/`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'X-CSRFToken': getCookie('csrftoken') }
+        })
+            .then(response => {
+                if (response.ok) {
+                    delete notificationQueue[slug]; // Remove from queue
+                    listItem.remove(); // Remove from panel
+                    fetchNotifications(); // Refresh the notification list
+                } else {
+                    console.error('Failed to mark notification as read');
+                }
+            })
+            .catch(error => {
+                console.error('Error marking notification as read:', error);
+            });
+    }
+
+    function markAllAsRead() {
+        fetch('/inbox/notifications/mark-all-as-read/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'X-CSRFToken': getCookie('csrftoken')}
+        })
+            .then(response => {
+                if (response.ok) {
+                    notificationQueue = {}; // Clear the queue
+                    notificationMenu.innerHTML = ''; // Clear existing notifications
+                    fetchNotifications(); // Refresh the notification list
+                } else {
+                    console.error('Failed to mark all notifications as read');
+                }
+            })
+            .catch(error => {
+                console.error('Error marking all notifications as read:', error);
+            });
+    }
+
     function fetchNotifications() {
-        fetch('/inbox/notifications/api/all_list/')
+        fetch('/inbox/notifications/api/unread_list/')
             .then(response => response.json())
             .then(data => {
-                notificationPanel.innerHTML = "";
-                // console.log("=>"+notificationPanel.innerHTML)
-                notificationPanel.innerHTML = ''; // Clear existing notifications
-                if (data.all_list) {
-                    generateNotificationPanel(data.all_list);
+                // Update notification count
+                 unread_count = data.unread_count;
+                if (unread_count > 0) {
+                    notificationCount.textContent = unread_count > 99 ? '99+' : unread_count;
+                    notificationCount.style.display = 'block';
+                    notificationMenu.style.maxHeight = "none"; // adjust to max-height
+                    notificationMenu.style.overflowY = 'auto';
+
+                    // Add "Read All Link"
+                    if (read_all_link == null) {
+                        read_all_link = document.createElement('a');
+                        read_all_link.href = 'javascript:;';
+                        read_all_link.className = 'dropdown-item text-center';
+                        read_all_link.textContent = 'Read All';
+                        read_all_link.addEventListener('click', markAllAsRead);
+                        notificationMenu.appendChild(read_all_link);
+                    }
+
+                    // generate notification panel
+                    if (data.unread_list) {
+                        // Update queue with new notifications
+                        data.unread_list.forEach((notification, index) => {
+                            if (!notificationQueue[notification.slug] && Object.keys(notificationQueue).length <= max_notifications) {
+                                generateNotificationPanel(notification);
+                            }
+                        });
+                    } else {
+                        console.error('Failed to fetch notifications:', data.error);
+                    }
+
                 } else {
-                    console.error('Failed to fetch notifications:', data.error);
+                    //Remove "Read All Link"
+                    if(read_all_link != null)
+                        read_all_link.remove();
+                    read_all_link = null;
+
+                    notificationCount.textContent = 0;
+                    notificationCount.style.display = 'none';
+                    notificationMenu.style.maxHeight = '55px'; // adjust to original max-height
+                    notificationMenu.style.overflowY = 'none';
+
+                    // Add message if no notifications
+                    const noNotificationsMessage = document.createElement('p');
+                    noNotificationsMessage.textContent = 'no more message';
+                    notificationMenu.appendChild(noNotificationsMessage);
                 }
-                // console.log("after=>"+notificationPanel.innerHTML);
+
             })
             .catch(error => {
                 console.error('Error fetching notifications:', error);
             });
-        // console.log("init=>"+notificationPanel.innerHTML+" end")
     }
     fetchNotifications();
 });
