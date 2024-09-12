ae35e2deed01520541b0290b59e38474a6930ef2
vision <vsion.zhao@gmail.com>
2024-09-12 09:34:26 -0500

    chart api

---
+++ home/forms/portfolio.py
@@ -0,0 +1,22 @@
+from django import forms
+from apps.common.models import *
+
+class PortfolioForm(forms.ModelForm):
+    class Meta:
+        model = Portfolio
+        fields = ['name']
+
+class PortfolioItemForm(forms.ModelForm):
+    class Meta:
+        model = PortfolioItem
+        fields = ['symbol', 'quantity', 'average_price']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['transaction_type', 'quantity', 'price', 'date']
+
+class PositionSizingForm(forms.ModelForm):
+    class Meta:
+        model = PositionSizing
+        fields = ['capital', 'risk', 'rounding', 'commission', 'tax']
