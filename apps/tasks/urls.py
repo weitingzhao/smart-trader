96d8050403ed6dc4ef00b5f6a626b45f04826091
vision <vsion.zhao@gmail.com>
2024-11-04 11:55:36 -0600

    quote stock page for Buy and Sell order and action

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('tasks/output/'              , views.task_output, name="task-output"),
     path('tasks/log/'                 , views.task_log,    name="task-log"),
     path('download-log-file/<str:file_path>/', views.download_log_file, name='download_log_file'),
+
+    path('tasks/symbol/refresh/'  , views.run_task,    name="run-task"),
 ]
