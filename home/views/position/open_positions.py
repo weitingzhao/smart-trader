87d64a5fd6f9c52704613f6b97a585503d35b759
vision <vsion.zhao@gmail.com>
2024-11-18 19:48:01 -0600

    open position

--- home/views/position/open_positions.py
+++ home/views/position/open_positions.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from home.templatetags.home_filter import order_price
 from django.db.models import (
-    F,Case, When, Value, IntegerField, Sum, Max,
+    F,Case, When, Value, IntegerField, Sum, Max,Min,
     FloatField, Q, BooleanField,Subquery, OuterRef)
 from logics.logic import Logic
 from apps.common.models import *
@@ -22,14 +22,15 @@ def default(request):
         return JsonResponse({'success': False, 'error': 'Default portfolio not found'}, status=404)
 
     holdings = Holding.objects.filter(portfolio=portfolio)
-
     # Extract symbols from portfolio items
     symbols = [item.symbol.symbol for item in holdings]
+    # Retrieve the UserStaticSetting record for the given user_id
+    user_static_setting = get_object_or_404(UserStaticSetting, user_id=user_id)
+    expect_gain_risk_ratio = user_static_setting.expect_gain_risk_ratio
 
     if len(symbols) > 0:
         # get current time previous day benchmark
         benchmark = instance.research.treading().get_stock_hist_bars(True, symbols, 2)
-
         # current time is base on current time is stock market open hour or close hour
         # if is close hour.
         #   a. if day data is not available, need use min data.
@@ -58,14 +59,43 @@ def default(request):
         initial_sell_orders_df = pd.DataFrame(list(initial_sell_orders))
         # Rename columns for clarity
         initial_sell_orders_df.rename(columns={
-            'order_place_date': 'initial_order_place_date',
-            'price_stop': 'initial_stop',
-            'price_limit': 'initial_limit'
+            'order_place_date': 'init_order_place_date',
+            'price_stop': 'init_stop',
+            'price_limit': 'init_limit'
         }, inplace=True)
         # Merge the initial sell orders data into holdings_df
         holdings_df = pd.merge(holdings_df, initial_sell_orders_df, on='holding_id', how='left')
 
-        # Step 2. Calculate dataframes
+        # Step 2. Get the last sell orders for each holding
+        # Subquery to get the maximum trade_id for each holding_id
+        max_trade_id_subquery = (
+            HoldingSellOrder.objects.filter(holding_id=OuterRef('holding_id'))
+            .order_by('-trade_id').values('trade_id'))[:1]
+        # Query to get holding_sell_order where trade_id is in the previous trade_id list
+        sell_orders_with_max_trade_id = (
+            HoldingSellOrder.objects.filter(trade_id__in=Subquery(max_trade_id_subquery)))
+        # Subquery to get the maximum holding_sell_order_id for each trade_id
+        max_sell_order_id_subquery = (
+            sell_orders_with_max_trade_id
+            .values('trade_id')
+            .annotate(max_sell_order_id=Max('holding_sell_order_id'))
+            .values('max_sell_order_id'))
+        # Query to get all holding_sell_order in the previous holding_sell_order_id list
+        last_sell_orders = HoldingSellOrder.objects.filter(
+            holding_sell_order_id__in=Subquery(max_sell_order_id_subquery)
+        ).values('holding_id', 'order_place_date', 'price_stop', 'price_limit')
+        # Convert the query result to a DataFrame
+        last_sell_orders_df = pd.DataFrame(list(last_sell_orders))
+        # Rename columns for clarity
+        last_sell_orders_df.rename(columns={
+            'order_place_date': 'current_order_place_date',
+            'price_stop': 'current_stop',
+            'price_limit': 'current_limit'
+        }, inplace=True)
+        # Merge the last sell orders data into holdings_df
+        holdings_df = pd.merge(holdings_df, last_sell_orders_df, on='holding_id', how='left')
+
+        # Step 3. Calculate dataframes
         # Merge the DataFrames on the 'symbol' column
         merged_df = pd.merge(latest_bar_df, benchmark_df, on='symbol', suffixes=('', '_bk'))
         # Merge items DataFrame with merged_df on 'symbol'
@@ -73,71 +103,89 @@ def default(request):
 
         # Fetch and sum the quantity_final from transaction
         transaction = (Transaction.objects.filter(holding__in=holdings)
-                        .annotate(
-                           trade_id=Case(
-                               When(buy_order_id__isnull=False, then=F('buy_order__trade_id')),
-                               When(sell_order_id__isnull=False, then=F('sell_order__trade_id')),
-                               default=Value(None),
-                               output_field=IntegerField()
-                           ),
-                           is_finished=Case(
-                               When(buy_order_id__isnull=False, then=F('buy_order__trade__is_finished')),
-                               When(sell_order_id__isnull=False, then=F('sell_order__trade__is_finished')),
-                               default=Value(None),
-                               output_field=BooleanField()
-                           )
-                       )
-                       .filter(Q(is_finished=False) | Q(is_finished__isnull=True))
-                       .annotate(amount=F('quantity_final') * F('price_final'))
-                       .values('holding_id')
-                       .annotate(
-                            quantity=Sum('quantity_final'),
-                            total_cost=Sum('amount'),
-                            average_price=Sum('amount') / Sum('quantity_final')
-                       ))
+            .annotate(
+               trade_id=Case(
+                   When(buy_order_id__isnull=False, then=F('buy_order__trade_id')),
+                   When(sell_order_id__isnull=False, then=F('sell_order__trade_id')),
+                   default=Value(None),
+                   output_field=IntegerField()
+               ),
+               is_finished=Case(
+                   When(buy_order_id__isnull=False, then=F('buy_order__trade__is_finished')),
+                   When(sell_order_id__isnull=False, then=F('sell_order__trade__is_finished')),
+                   default=Value(None),
+                   output_field=BooleanField()
+               )
+           )
+           .filter(Q(is_finished=False) | Q(is_finished__isnull=True))
+           .annotate(amount=F('quantity_final') * F('price_final'))
+           .values('holding_id')
+           .annotate(
+                init_transaction_id = Min('transaction_id'),
+                quantity=Sum('quantity_final'),
+                total_cost=Sum('amount'),
+                average_price=Sum('amount') / Sum('quantity_final')
+           ))
         transaction_df = pd.DataFrame(list(transaction),
-                                      columns=['holding_id', 'quantity', 'total_cost', 'average_price'])
-
+            columns=['holding_id', 'init_transaction_id', 'quantity', 'total_cost', 'average_price'])
+        # Retrieve the transaction record
+        init_transaction_ids = transaction_df['init_transaction_id'].tolist()
+        initial_transactions = (
+            Transaction.objects.filter(transaction_id__in=init_transaction_ids)
+            .values('holding_id', 'quantity_final', 'price_final')
+        )
+        # Convert the query result to a DataFrame and rename fields
+        initial_transactions_df = pd.DataFrame(list(initial_transactions))
+        initial_transactions_df.rename(columns={'quantity_final': 'init_quantity','price_final': 'init_price'}, inplace=True)
+        initial_transactions_df['init_inv'] = initial_transactions_df['init_quantity'] * initial_transactions_df['init_price']
         # Merge action_df with final_df
         final_df = pd.merge(final_df, transaction_df, left_on='holding_id', right_on='holding_id', how='inner').fillna(0)
+        final_df = pd.merge(final_df, initial_transactions_df, on='holding_id', how='left')
 
-        # Step 3. Calculate the total cost & market value
+        # Step 4. Calculate the total cost & market value
         final_df['market_value'] = final_df['quantity'] * final_df['close']
 
-        # Step 4. Calculate the change since last biz day
-        # Calculate the change as the difference between latest_bar.close and benchmark.close
+        # Step 5. Calculate daily change in position & percent
         final_df['chg'] = final_df['close'] - final_df['close_bk']
-        # Calculate change in position
         final_df['chg_position'] = final_df['quantity'] * final_df['chg']
-        # Calculate the change percent
         final_df['chg_pct'] = ((final_df['chg'] / final_df['close_bk']) * 100).round(2)
-        # Add a new column 'trend' based on the 'change' column
         final_df['trend'] = final_df['chg'].apply(lambda x: "UP" if x > 0 else ("DOWN" if x < 0 else "-"))
 
-        # Step 6. Calculate the total cost & change
-        # Calculate total change in value
-        final_df['total_chg_position'] = final_df.apply(lambda row: (Decimal(row['market_value']) - Decimal(row['total_cost'])).quantize(Decimal('0.01')), axis=1)
+        # Step 6. Calculate Risk vs Margin
+        # Gain
+        final_df['gain_pct'] =(final_df['close'].apply(float) - final_df['average_price'].apply(float)) / final_df['average_price'].apply(float) * 100
+        final_df['gain'] = final_df['average_price'].apply(float) * final_df['quantity'].apply(float) * final_df['gain_pct'] / 100
+        final_df['gain_trend'] = final_df['gain'].apply(lambda x: "UP" if x > 0 else ("DOWN" if x < 0 else "-"))
+        # Risk
+        final_df['risk_pct'] = ((final_df['current_stop'].apply(float) + final_df['current_limit'].apply(float)) / 2 - final_df['average_price'].apply(float)) / final_df['average_price'].apply(float) * 100
+        final_df['risk'] = final_df['average_price'].apply(float) * final_df['quantity'].apply(float) * final_df['risk_pct'] / 100
+        final_df['risk_trend'] = final_df['risk'].apply(lambda x: "UP" if x > 0 else ("DOWN" if x < 0 else "-"))
+        # Distance
+        final_df['dist'] = final_df['gain'] - final_df['risk']
+        final_df['dist_pct'] = final_df['dist'].apply(float) / final_df['total_cost'].apply(float) * 100
 
-        # Calculate total change percentage
-        final_df['total_chg_pct'] = final_df.apply(lambda row: (
-            (Decimal(row['total_chg_position']) / Decimal(row['total_cost']) * 100).quantize(Decimal('0.01')) if row['total_cost'] != 0 else Decimal('0.00')), axis=1)
-        # Calculate total change trand
-        final_df['total_trend'] = final_df['total_chg_position'].apply(lambda x: "UP" if x > 0 else ("DOWN" if x < 0 else "-"))
+        # Step 7. Calculate Goal
+        final_df['init_risk'] = final_df['init_quantity'] * (final_df['init_price'] - (final_df['init_stop'] + final_df['init_limit']) / 2)
+        final_df['init_risk_pct'] = final_df['init_risk'] / final_df['init_inv'] * 100
 
-
-
-        def format(x):
-            if pd.isna(x):
-                return '-'
-            return f"+{round(x, 2)}" if x > 0 else (f"-{abs(round(x, 2))}" if x < 0 else round(x, 2))
-        # Format the change & change values
-        final_df['chg_position'] = final_df['chg_position'].apply(format)
-        final_df['chg'] = final_df['chg'].apply(format)
-        final_df['total_chg_position'] = final_df['total_chg_position'].apply(format)
-        final_df['total_chg_pct'] = final_df['total_chg_pct'].apply(format)
+        # *expect_gain_risk_ratio
 
         # Convert the DataFrame to JSON
         final_json = final_df.to_json(orient='records', date_format='iso')
+
+        # Assuming final_df is already defined and populated
+        final_df['market_value_bk'] = final_df['close_bk'] * final_df['quantity']
+
+        mv = final_df['market_value'].sum()
+        mv_bk = final_df['market_value_bk'].sum()
+
+        mv_chg_pct = (mv - mv_bk) / mv_bk * 100
+
+        summary = {
+            'mv': mv,
+            'mv_chg_pct': mv_chg_pct,
+            # Add other context variables here
+        }
     else:
         final_json = []
 
@@ -149,6 +197,7 @@ def default(request):
             'segment': 'open_positions',
             'portfolio': portfolio,
             'portfolio_items': final_json,
+            'summary': summary
         })
 
 def get_holding_buy_order(request, holding_buy_order_id):
