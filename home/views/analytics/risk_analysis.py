21d8d14e427f6419fb5ed626d901cfbff11afaff
vision <vsion.zhao@gmail.com>
2024-12-20 10:16:10 -0600

    risk analysis

--- home/views/analytics/risk_analysis.py
+++ home/views/analytics/risk_analysis.py
@@ -1,11 +1,215 @@
+import pandas as pd
+from logics.logic import Logic
 from django.shortcuts import render
+from apps.common.models import *
+from django.http import JsonResponse
+from django.shortcuts import render
+import datetime  # For datetime objects
+import os.path  # To manage paths
+import sys  # To find out the script name (in argv[0])
 
+import matplotlib
+matplotlib.use('Agg')  # Set non-interactive backend before importing pyplot
+import matplotlib.pyplot as plt
+import io
+import base64
+
+instance = Logic()
+
+import backtrader as bt
 
 def default(request):
+
+    user_id = request.user.id  # Assuming you have the user_id from the request
+
+    # Step 0. Get default portfolio
+    portfolio = Portfolio.objects.filter(user=user_id, is_default=True).order_by('-portfolio_id').first()
+    if not portfolio:
+        return JsonResponse({'success': False, 'error': 'Default portfolio not found'}, status=404)
+
+
+    symbol = 'OWL'
+    stock_data = MarketStockHistoricalBarsByDay.objects.filter(symbol=symbol).order_by('time')
+
+    # Convert the QuerySet to a DataFrame
+    stock_data_df = pd.DataFrame(list(stock_data.values()))
+
+    # Ensure the DataFrame has the required columns for Backtrader
+    stock_data_df['datetime'] = pd.to_datetime(stock_data_df['time'])
+    stock_data_df.set_index('datetime', inplace=True)
+    stock_data_df = stock_data_df[['open', 'high', 'low', 'close', 'volume']]
+
+    # Create a cerebro entity
+    cerebro = bt.Cerebro()
+
+    # Add a strategy
+    # strats = cerebro.optstrategy(TestStrategy, map_period=range(10, 31))
+
+    strats = cerebro.addstrategy(TestStrategy)
+
+    data = bt.feeds.PandasData(dataname=stock_data_df)
+
+    # Datas are in a subfolder of the samples. Need to find where the script is
+    # because it could have been called from anywhere
+    # modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    # datapath = os.path.join(modpath, 'datas/orcl-1995-2014.txt')
+    # # Create a Data Feed
+    # data = bt.feeds.YahooFinanceCSVData(
+    #     dataname=datapath,
+    #     # Do not pass values before this date
+    #     fromdate=datetime.datetime(2000, 1, 1),
+    #     # Do not pass values after this date
+    #     todate=datetime.datetime(2000, 12, 31),
+    #     reverse=False)
+
+
+
+    # Add the Data Feed to Cerebro
+    cerebro.adddata(data)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(1000.0)
+
+    # Add a FixedSize sizer according to the stake
+    cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+    # Set the Commission - 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Save the plot as an image
+    chart = plot_cerebro(cerebro)
+
+
     return render(
         request=request,
         template_name='pages/analytics/risk_analysis.html',
         context= {
             'parent': 'analytics',
             'segment': 'risk_analysis',
+            'chart': chart,
         })
+
+def plot_cerebro(cerebro):
+    # Generate plot
+    fig = cerebro.plot()[0][0]  # Returns a Matplotlib figure
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')  # Save the figure to a buffer
+    buf.seek(0)  # Move to the beginning of the buffer
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')  # Convert to base64
+    buf.close()
+    plt.close(fig)  # Close the figure to free memory
+    return image_base64
+
+class TestStrategy(bt.Strategy):
+    params = (
+        ('map_period', 10),
+        ('printlog', False),
+    )
+
+
+    def log(self, txt, dt=None, doprint=False):
+        ''' Logging function for this strategy'''
+        if self.params.printlog or doprint:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
+
+
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.data_close = self.datas[0].close
+
+        # To keep track of pending orders
+        self.order = None
+        self.buy_price = None
+        self.buy_comm = None
+        self.bar_executed = 0
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.map_period)
+
+        # Indicators for the plotting show
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
+        bt.indicators.StochasticSlow(self.datas[0])
+        bt.indicators.MACDHisto(self.datas[0])
+
+        rsi = bt.indicators.RSI(self.datas[0])
+        bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        bt.indicators.ATR(self.datas[0], plot=False)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+                self.buy_price = order.executed.price
+                self.buy_comm = order.executed.comm
+            else: # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # Write down: no pending order
+        self.order = None
+
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.data_close[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+            # Not yet ... we MIGHT BUY if ...
+            if self.data_close[0] > self.sma[0]:
+                # BUY, BUY, BUY!!! (with all possible default parameters)
+                self.log('BUY CREATE, %.2f' % self.data_close[0])
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.buy()
+        else:
+            if self.data_close[0] < self.sma[0]:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.data_close[0])
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+
+
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.map_period, self.broker.getvalue()), doprint=True)
